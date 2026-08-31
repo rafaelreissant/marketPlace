cf6d3983f2933bb70e82f6fbc622e1ddb1dc7d88
rafaelreissant <santireis14@gmail.com>
2026-08-31 18:03:48 -0300

    Adding repository

---
+++ marketPlace/src/main/java/com/example/marketPlace/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.marketPlace.Repository;
+
+import com.example.marketPlace.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
